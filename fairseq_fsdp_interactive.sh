65176bb06ff08c5f8176e8fafa542c38f6ce9b36
Pavel Belevich <belevichp@gmail.com>
2021-08-24 01:05:49 +0000

    --cpus-per-task=96

--- fairseq_fsdp_interactive.sh
+++ fairseq_fsdp_interactive.sh
@@ -7,6 +7,7 @@ srun --label \
     --job-name=fairseq-fsdp \
     --nodes=8 \
     --gpus=64 \
+    --cpus-per-task=96 \
     --partition=train \
     --time=24:00:00 \
     fairseq-train data-bin/wikitext-103 \
